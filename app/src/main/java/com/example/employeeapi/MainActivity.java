a37f997b3198bba8ca2d2fe1a4bca180eac9a896
unknown <IvyLeano@outlook.com>
2019-10-29 16:52:26 -0400

    rest api done

--- app/src/main/java/com/example/employeeapi/MainActivity.java
+++ app/src/main/java/com/example/employeeapi/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.employeeapi;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.android.volley.Response;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import org.json.JSONObject;
+import java.util.Vector;
 import android.os.Bundle;
+import org.json.JSONArray;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,20 +27,19 @@ public class MainActivity extends AppCompatActivity {
 
         // 2. construct request
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                Request.Method.GET, baseUrl + api, null, new Response.Listener<JSONObject>() {
+                Request.Method.GET, baseUrl, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
                     // 2.a. response logic - store response to a vector array
                     Vector newsData = new Vector();
                     JSONArray array = response.getJSONArray("articles");
-
+                    System.out.println(array);
                     for (int i = 0; i < array.length(); i++) {
-                        NewsData news = new NewsData();
-                        news.setData(array.getJSONObject(i));
-                        newsData.add(news);
+
+
                     }
-                    setView(newsData);
+
                 } catch (Exception e) {
                     System.out.println("Logged from JsonObjectRequest() in NewsRestApi.java: " + e);
                 }
